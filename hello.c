fe0bfa8c402a95d31a90db0737447715d97ab432
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-02 17:59:38 +0000

    Mon, Oct 2, 2023, 12:59 PM -05:00

--- hello.c
+++ hello.c
@@ -3,9 +3,12 @@
 int main(void)
 {
     printf("Hi! I am Askbot, the amazing bot who asks you fun questions! Get ready to type in your answer to the question, and then press 'enter' to go to the next one!\n");
-    string animal = get_string("What is your favorite animal?\n");
+    string animal = get_string("What's your favorite animal?\n");
     printf("Nice! My favorite animal is a snow leopard, but I think that a(n) %s is cool too.\n", animal);
     string song_artist = get_string("Who is your favorite music artist?\n");
     string song_title = get_string("What is your favorite song by them?\n");
-    printf("I actually don't like %s's song called '%s', but I do like 'Lily' by Alan Walker.\n", song_title, song_artist);
-}
+    printf("I actually really hate %s's song called '%s', but I LOVE the song 'Lily' by Alan Walker.\n", song_artist, song_title);
+    string number = get_string("What's your favorite number?\n");
+    printf("My favorite number is %s as well!\n", number);
+    printf("That's it for now! Thanks for answering!\n\n\n ***Data collected successfully.*** \n\n\n Selling it to the North Korean government... Processing... Complete!");
+    }
