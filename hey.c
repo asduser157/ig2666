b5ca690359fac12a9af387bc0c2eb6f2fb0b6266
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-04 15:12:05 +0000

    Wed, Oct 4, 2023, 10:12 AM -05:00

--- hey.c
+++ hey.c
@@ -1,27 +1,15 @@
-#include <studio.h>
+#include <stdio.h>
 #include <cs50.h>
 int main(void)
 {
     char c = get_char("Do you agree to us collecting your data and selling it to the North Korean government?\n");
 
-    if (c == 'Y' || 'y')
+    if (c == 'Y' || c == 'y')
     {
-        printf("You have agreed.\n")
+        printf("You have agreed.\n");
     }
-    if (c == 'n' || 'N')
+    if (c == 'n' || c == 'N')
     {
-        printf("You have not agreed, so we will not only collect your data, but your family's data too. It's too bad you didn't agree the first time!")
-    }b#include <studio.h>
-#include <cs50.h>
-int main(void)
-{
-    char c = get_char("Do you agree to us collecting your data and selling it to the North Korean government?\n");
-
-    if (c == 'Y' || 'y')
-    {
-        printf("You have agreed.\n")
+        printf("You have not agreed, so we will not only collect your data, but your family's data too. It's too bad you didn't agree the first time!\n");
     }
-    if (c == 'n' || 'N')
-    {
-        printf("You have not agreed, so we will not only collect your data, but your family's data too. It's too bad you didn't agree the first time!")
-    }
+}
