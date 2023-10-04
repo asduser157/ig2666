7d09282f1111700a3b2cfd5a301fa5806773fa24
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-04 15:09:28 +0000

    Wed, Oct 4, 2023, 10:09 AM -05:00

---
+++ hey.c
@@ -0,0 +1,27 @@
+#include <studio.h>
+#include <cs50.h>
+int main(void)
+{
+    char c = get_char("Do you agree to us collecting your data and selling it to the North Korean government?\n");
+
+    if (c == 'Y' || 'y')
+    {
+        printf("You have agreed.\n")
+    }
+    if (c == 'n' || 'N')
+    {
+        printf("You have not agreed, so we will not only collect your data, but your family's data too. It's too bad you didn't agree the first time!")
+    }b#include <studio.h>
+#include <cs50.h>
+int main(void)
+{
+    char c = get_char("Do you agree to us collecting your data and selling it to the North Korean government?\n");
+
+    if (c == 'Y' || 'y')
+    {
+        printf("You have agreed.\n")
+    }
+    if (c == 'n' || 'N')
+    {
+        printf("You have not agreed, so we will not only collect your data, but your family's data too. It's too bad you didn't agree the first time!")
+    }
