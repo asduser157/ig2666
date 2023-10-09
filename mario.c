f5592e9811560801fcafccc7f76705ca7db268c9
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-09 15:19:48 +0000

    Mon, Oct 9, 2023, 10:19 AM -05:00

--- mario.c
+++ mario.c
@@ -1,10 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
 int main(void)
 {
-    for(int row = 0 row < 3 row++)
+    for(int row = 0, row < 3, row++)
     {
         for(int column = 0, column < 3, column++)
         {
-            
+            printf("#");
         }
+        printf("\n")
     }
 }
