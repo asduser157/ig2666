62ce86e7d2c8556a1244329564529abe28374cb1
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 22:12:36 +0000

    Wed, Oct 11, 2023, 5:12 PM -05:00

--- llama.c
+++ llama.c
@@ -25,7 +25,7 @@ safety_net(int n1, int n2, int ans1, int ans2, int num, int ans3, int final_ans)
     }
 }
 
-int main(void);
+int main(void)
 {
     safety_net(n1, n2, ans1, ans2, num, ans3, final_ans);
 }
