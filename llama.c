abc7904894974fe3d35d93730a393e08bd671e14
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 15:32:31 +0000

    Wed, Oct 11, 2023, 10:32 AM -05:00

--- llama.c
+++ llama.c
@@ -3,15 +3,17 @@
 
 int main(void)
 {
-    long n1 = get_long("Population Start Size:\n");
+    int n1 = get_int("Population Start Size: ");
     while (n1 > 9)
     {
         printf("Once upon a time, there were %ld llamas in Llamaland.\n\n", n1);
-        long n2 = get_long("Population End Size:\n");
-        while (n2 > n1)
-        {
-            printf("A while later, the population had grown to %ld llamas in Llamaland.\n", n2);
-        }
+        int n2 = get_int("Population End Size: ");
+    }
+    if (n2 > n1)
+    {
+        printf("A while later, the population had grown to %ld llamas in Llamaland.\n\n", n2);
+        int n3 = get_int("How many years did it take for the llama popluation to grow to %ld?", n2);
+
     }
 
 }
