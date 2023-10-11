0e9ed153490ce29f991c6386ae151b12fc8a5856
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 22:38:04 +0000

    Wed, Oct 11, 2023, 5:38 PM -05:00

--- llama.c
+++ llama.c
@@ -7,13 +7,15 @@ int main(void)
     while (n1 > 9)
     {
         int n2 = get_int("Population End Size: ");
-        printf("A while later, the population had grown to %i llamas in Llamaland.\n\n", n2);
-        int ans1 = n1 / 3;
-        int ans2 = n1 / 4;
-        int num = n2 - n1;
-        int ans3 = ans1 - ans2;
-        int final_ans = num / ans3;
-        printf("That population growth took %i years.\n", final_ans);
+        while (n2 > n1){
+            printf("A while later, the population had grown to %i llamas in Llamaland.\n\n", n2);
+            int ans1 = n1 / 3;
+            int ans2 = n1 / 4;
+            int num = n2 - n1;
+            int ans3 = ans1 - ans2;
+            int final_ans = num / ans3;
+            printf("That population growth took %i years.\n", final_ans);
+        }
     }
 }
 
