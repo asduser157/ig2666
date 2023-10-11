a15c57e3f57eb455f0b0c0a88772e7fd6a0f7841
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 15:18:07 +0000

    Wed, Oct 11, 2023, 10:18 AM -05:00

--- llama.c
+++ llama.c
@@ -1,7 +1,10 @@
 #include <cs50.h>
-#include <stdio.c>
+#include <stdio.h>
 
 int main(void)
 {
-    long n = get_long("Population Start Size:\n")
+    long n1 = get_long("Population Start Size:\n");
+    printf("Once upon a time, there were %ld llamas.\n", n1);
+    long n2 = get_long()
+
 }
