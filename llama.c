b4ffb6d947e05e1e7fdfea689f9a28933fbb967f
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 15:23:03 +0000

    Wed, Oct 11, 2023, 10:23 AM -05:00

--- llama.c
+++ llama.c
@@ -4,7 +4,13 @@
 int main(void)
 {
     long n1 = get_long("Population Start Size:\n");
-    printf("Once upon a time, there were %ld llamas.\n", n1);
-    long n2 = get_long()
+    while (n1 > 9)
+    {
+        printf("Once upon a time, there were %ld llamas.\n", n1);
+        long n2 = get_long("Population End Size:\n");
+        while (n2 > n1)
+        {
+            printf("Somehow, )
+        }
 
 }
