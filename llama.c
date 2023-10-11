e84679c465b92bdf76f1e1713d48b47a55ecc4a1
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 22:34:35 +0000

    Wed, Oct 11, 2023, 5:34 PM -05:00

--- llama.c
+++ llama.c
@@ -6,13 +6,13 @@ int main(void)
     int n1 = get_int("Population Start Size: ");
     if (n1 > 9)
     {
-        n2 = get_int("Population End Size: ");
+        int n2 = get_int("Population End Size: ");
         printf("A while later, the population had grown to %i llamas in Llamaland.\n\n", n2);
-        ans1 = n1 / 3;
-        ans2 = n1 / 4;
-        num = n2 - n1;
-        ans3 = ans1 - ans2;
-        final_ans = num / ans3;
+        int ans1 = n1 / 3;
+        int ans2 = n1 / 4;
+        int num = n2 - n1;
+        int ans3 = ans1 - ans2;
+        int final_ans = num / ans3;
         printf("That population growth took %i years.\n", final_ans);
     }
     else
