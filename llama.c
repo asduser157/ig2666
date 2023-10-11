c37a922d8ee6dc82aee08d0a7f5a025fbfd81043
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 21:38:10 +0000

    Wed, Oct 11, 2023, 4:38 PM -05:00

--- llama.c
+++ llama.c
@@ -1,11 +1,30 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int main(void)
+
+
 void
-ensure()
+safety_net()
+{
+    int n1 = get_int("Population Start Size: ");
+    if (n1 > 9)
+    {
+        ensure1();
+    }
+    else
+    {}
+}
+
+void
+ensure1()
 {
     printf("Once upon a time, there were %i llamas in Llamaland.\n\n", n1);
     int n2 = get_int("Population End Size: ");
+}
+void
+ensure2()
+{
     printf("A while later, the population had grown to %i llamas in Llamaland.\n\n", n2);
     int ans1 = n1 / 3;
     int ans2 = n1 / 4;
@@ -13,16 +32,6 @@ ensure()
     int ans3 = ans1 - ans2;
     int final_ans = num / ans3;
     printf("That population growth took %i years.\n", final_ans);
-}
-
-
-int main(void)
-{
-    int n1 = get_int("Population Start Size: ");
-    if (n1 > 9)
-    {
-        ensure
-    }
 }
 
 
