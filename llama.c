d2b2f83ccad94688c6c8a6800cd990afa7da2fd3
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 21:41:29 +0000

    Wed, Oct 11, 2023, 4:41 PM -05:00

--- llama.c
+++ llama.c
@@ -27,13 +27,20 @@ ensure1()
 void
 ensure2()
 {
-    printf("A while later, the population had grown to %i llamas in Llamaland.\n\n", n2);
-    int ans1 = n1 / 3;
-    int ans2 = n1 / 4;
-    int num = n2 - n1;
-    int ans3 = ans1 - ans2;
-    int final_ans = num / ans3;
-    printf("That population growth took %i years.\n", final_ans);
+    if (n2 > n1)
+    {
+        printf("A while later, the population had grown to %i llamas in Llamaland.\n\n", n2);
+        int ans1 = n1 / 3;
+        int ans2 = n1 / 4;
+        int num = n2 - n1;
+        int ans3 = ans1 - ans2;
+        int final_ans = num / ans3;
+        printf("That population growth took %i years.\n", final_ans);
+    }
+    else
+    {
+        ensure1();
+    }
 }
 
-
+safety_net();
