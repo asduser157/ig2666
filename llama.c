a8b677b4fd2d0728663e3a532a98032171133063
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 23:32:33 +0000

    Wed, Oct 11, 2023, 6:32 PM -05:00

--- llama.c
+++ llama.c
@@ -8,7 +8,8 @@ int main()
     do {
         printf("Please enter the starting population size of llamas, ensuring your input is a number greater than 9 and less than 10,000: ");
 
-        if (scanf ("%d", &n1) != 1) {
+        if (scanf ("%d", &n1) != 1)
+        {
             printf("Please enter the starting population size of llamas, ensuring your input is a number greater than 9 and less than 10,000: ");
             while (getchar() != '\n');
         }
@@ -16,11 +17,13 @@ int main()
         {
             printf("Input is out of range. Please enter a number between 9 and 10,000: ");
         } while (n1 < 9 || n1 > 10000);
-    }
-    do  {
-        printf("Please enter the ending population size of llamas, ensuring your input is a number greater than the previous number you entered and less than or equal to 10,000:");
+        }
 
-        if (scanf ("%d", &n2) != 1) {
+    do  {
+        printf("Please enter the ending population size of llamas, ensuring your input is a number greater than the previous number you entered and less than or equal to 10,000: ");
+
+        if (scanf ("%d", &n2) != 1)
+        {
             printf("Please enter the ending population size of llamas, ensuring your input is a number greater than the previous number you entered and less than 10,000: ");
             while (getchar() != '\n');
         }
@@ -29,7 +32,7 @@ int main()
             printf("Input is out of range. Please enter a number greater than the previous number you entered and less than 10,000: ");
         } while (n1 < 9 || n1 > 10000);
     }  return 0;
-      }
+}
     printf("A while later, the population had grown to %i llamas in Llamaland.\n\n", n2);
     int ans1 = n1 / 3;
     int ans2 = n1 / 4;
