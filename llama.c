54618bdbcb34c28d540ed4421d5a4c759e370fb5
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 22:57:52 +0000

    Wed, Oct 11, 2023, 5:57 PM -05:00

--- llama.c
+++ llama.c
@@ -13,4 +13,30 @@ int main(void)
     int final_ans = num / ans3;
     printf("That population growth took %i years.\n", final_ans);
 }
+#include <stdio.h>
 
+int main() {
+    int number;
+    int min = 1; // Minimum allowed value
+    int max = 100; // Maximum allowed value
+
+    do {
+        printf("Please enter an integer between %d and %d: ", min, max);
+
+        // Read the integer input from the user
+        if (scanf("%d", &number) != 1) {
+            // Input is not a valid integer
+            printf("Invalid input. Please enter a valid integer.\n");
+            // Clear the input buffer
+            while (getchar() != '\n');
+        }
+        else if (number < min || number > max) {
+            // Input is outside the specified range
+            printf("Input is out of range. Please enter a number between %d and %d.\n", min, max);
+        }
+    } while (number < min || number > max);
+
+    printf("You entered a valid integer: %d\n", number);
+
+    return 0;
+}
