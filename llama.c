c143bac40e7ed512934f5c44db15ab64471bf01c
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 22:33:43 +0000

    Wed, Oct 11, 2023, 5:33 PM -05:00

--- llama.c
+++ llama.c
@@ -1,11 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
-void
-safety_net(int n1, int n2, int ans1, int ans2, int num, int ans3, int final_ans)
+int main(void)
 {
-    n1 = get_int("Population Start Size: ");
+    int n1 = get_int("Population Start Size: ");
     if (n1 > 9)
     {
         n2 = get_int("Population End Size: ");
@@ -19,13 +17,7 @@ safety_net(int n1, int n2, int ans1, int ans2, int num, int ans3, int final_ans)
     }
     else
     {
-        safety_net(n1, n2, ans1, ans2, num, ans3, final_ans);
-    }
-}
 
-int main(void)
-{
-    safety_net(n1, n2, ans1, ans2, num, ans3, final_ans);
-    return 0;
+    }
 }
 
