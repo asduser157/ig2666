a41535a6f1094b1fc471c7f3faf3e35c40cdf817
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-11 15:26:52 +0000

    Wed, Oct 11, 2023, 10:26 AM -05:00

--- llama.c
+++ llama.c
@@ -6,11 +6,11 @@ int main(void)
     long n1 = get_long("Population Start Size:\n");
     while (n1 > 9)
     {
-        printf("Once upon a time, there were %ld llamas.\n", n1);
+        printf("Once upon a time, there were %ld llamas on Lily Island.\n", n1);
         long n2 = get_long("Population End Size:\n");
         while (n2 > n1)
         {
-            printf("Somehow, )
+            printf("A while later, the population had grown to %ld on)
         }
 
 }
