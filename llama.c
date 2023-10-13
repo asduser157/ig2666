4b520d0730e0d2ebfb93e5c1bce1e6dac59d26e7
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-13 14:49:57 +0000

    Fri, Oct 13, 2023, 9:49 AM -05:00

--- llama.c
+++ llama.c
@@ -40,18 +40,11 @@ int main()
         }
         else if (n2 > n1 && n2 < 10001){
         printf("\nA while later, the population had grown to %d llamas in Llamaland.\n", n2);
+        year = year + 1;
+        printf("\nThat population growth took %d years!\n", year);
         }
     }
     while (n2 < n1);
-    do {
-        int born = n1 / 3;
-        int dead = n1 / 4;
-        n2 = born - dead;
-        year ++;
-    }
-    while (n1 < n2);
-    return year;
-    printf("\nThat population growth took %d years!\n", year);
 
 
 }
