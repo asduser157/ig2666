d2d35a14b0f3a637773267decc8f34a7a3aa8d83
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-20 21:52:25 +0000

    Fri, Oct 20, 2023, 4:52 PM -05:00

--- credit.c
+++ credit.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#
+#include <string.h>
 #include <cs50.h>
 int main(void)
 {
     long card_number = get_long("Please enter a credit card number to test its validity.");
-    if card
+    
+
 }
