a779b48e43e7b8d6d7b622ca7a892806e34541a8
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-20 22:03:45 +0000

    Fri, Oct 20, 2023, 5:03 PM -05:00

--- credit.c
+++ credit.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <cs50.h>
+#include <stdlib.h>
 int main(void)
 {
     char card_string[17];
     long card_number = get_long("Please enter a credit card number to test its validity.");
     beginning_numbers = card_number % ();
     sprintf(card_string, "%d", card_number);
-    int length1 = strlen(card_string);
+    int length = strlen(card_string);
+
+
 
 
 
