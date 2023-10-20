746ba0aa2fdd432aeeb57e0769bb8752b19b8c50
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-20 22:23:20 +0000

    Fri, Oct 20, 2023, 5:23 PM -05:00

--- credit.c
+++ credit.c
@@ -6,19 +6,19 @@ int main(void)
 {
     char card_string[17];
     long card_number = get_long("Please enter a credit card number to test its validity.");
-    sprintf(card_string, "%d", card_number);
+    sprintf(card_string, "%ld", card_number);
     int length = strlen(card_string);
     long card_long = atol(card_string);
-    b = card_long % (length - 2);
-    if (b = "51" || b = "52" || b = "53" || b = "54" || b = "55")
+    int b = card_long % (length - 2);
+    if (b = 51 || b = 52 || b = 53 || b = 54 || b = 55)
     {
         printf("DISCOVER/MASTERCARD");
     }
-    else if (b = "40" || b = "41" || b = "42" || b = "43" || b = "44" || b = "45" || b = "46" || b = "47" || b = "48" || b = "49")
+    else if (b = 40 || b = 41 || b = 42 || b = 43 || b = 44 || b = 45 || b = 46 || b = 47 || b = 48 || b = 49)
     {
         printf("VISA");
     }
-    else if ( b = "34" || b = "37")
+    else if ( b = 34 || b = 37)
     {
         printf("AMERICAN EXPRESS");
     }
