eb9a762cfa36ceef720d93b2332d88f2f5f726b2
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-20 22:17:42 +0000

    Fri, Oct 20, 2023, 5:17 PM -05:00

--- credit.c
+++ credit.c
@@ -9,10 +9,19 @@ int main(void)
     sprintf(card_string, "%d", card_number);
     int length = strlen(card_string);
     long card_long = atol(card_string);
-    beginning_numbers = card_long % (length - 2);
-    if (beginning numbers = "
-
-
+    b = card_long % (length - 2);
+    if (b = "51" || b = "52" || b = "53" || b = "54" || b = "55")
+    {
+        printf("DISCOVER/MASTERCARD");
+    }
+    else if (b = "40" || b = "41" || b = "42" || b = "43" || b = "44" || b = "45" || b = "46" || b = "47" || b = "48" || b = "49")
+    {
+        printf("VISA");
+    }
+    else if ( b = "34" || b = "37")
+    {
+        printf("AMERICAN EXPRESS");
+    }
 
 
 
