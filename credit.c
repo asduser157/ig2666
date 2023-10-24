1b6bb9ad17d03570e41b92c3d93e77f9efe33f8d
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-24 16:18:08 +0000

    Tue, Oct 24, 2023, 11:18 AM -05:00

--- credit.c
+++ credit.c
@@ -115,8 +115,6 @@ int main(void)
     else if (b == 40 || b == 41 || b == 42 || b == 43 || b == 44 || b == 45 || b == 46 || b == 47 || b == 48 || b == 49)
     {
     printf("VISA\n");
-    printf("%d\n", step2);
-    printf("%d", step3);
         if (step3 == 0)
         {
             printf("***VALID CREDIT CARD NUMBER***\n");
