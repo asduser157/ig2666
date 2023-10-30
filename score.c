f99cd93bfd62e6ff00795d67ea5c4e933410dd79
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-10-30 15:04:11 +0000

    Mon, Oct 30, 2023, 10:04 AM -05:00

---
+++ score.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+    float score1 = 95.6;
+    float score2 = 88.9;
+    int score3 = 100.0;
+    float avg1 = ((score1 + score2 + score3) / 3.0);
+    printf("Average Grade: %f\n", avg1);
+
+}
