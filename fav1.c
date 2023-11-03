5a365202902698a1f9c4517b7fb7bba6c1922adb
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-03 15:02:20 +0000

    Fri, Nov 3, 2023, 10:02 AM -05:00

--- fav1.c
+++ fav1.c
@@ -2,10 +2,14 @@
 #include<cs50.h>
 void favorite() {
     string array1[4];
-    string one = get_string("What is your favorite color?\n");
+    string one = get_string("Your favorite color is:\n");
     printf("No way! My favorite color is %s, too!\n", one);
-    string two = get_string("What is your favorite movie?\n");
+    string two = get_string("Your favorite movie is called:\n");
     printf("Really? You should know that The Princess Bride is better than %s!\n", two);
+    string three = get_string("Your favorite animal is a/an:\n");
+    printf("I forgot about %ss! Thanks for helping me remember that amazing animal!\n", three);
+    string four = get_string("Your favorite dance move is known as:\n");
+    printf("No way! My favorite color is %s, too!\n", one);
     }
 int main(void)
 {
