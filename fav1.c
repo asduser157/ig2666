cf9ce46e70ce18ea90f77976139deef412d8903f
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-03 15:04:43 +0000

    Fri, Nov 3, 2023, 10:04 AM -05:00

--- fav1.c
+++ fav1.c
@@ -9,7 +9,7 @@ void favorite() {
     string three = get_string("Your favorite animal is a/an:\n");
     printf("I forgot about %ss! Thanks for helping me remember that amazing animal!\n", three);
     string four = get_string("Your favorite dance move is known as:\n");
-    printf("No way! My favorite color is %s, too!\n", one);
+    printf("I think that %s is just okay. The moonwalk, on the other hand, is out of this world.\n", four);
     }
 int main(void)
 {
