ecd59e3798db71c61276dabb8695568ba3a590a3
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-08 16:17:03 +0000

    Wed, Nov 8, 2023, 10:17 AM -06:00

--- length.c
+++ length.c
@@ -1,1 +1,11 @@
-int main(void)
+#include stdio.c
+#include int main(void)
+{
+    string name = get_string("What is yoyur first name\n");
+    int num = 0;
+    while (name[n] != '\0';)
+    {
+        n = n + 1;
+    }
+    printf("%i", num);
+}
