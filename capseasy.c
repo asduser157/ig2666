dfb887ce12d05a6af73bd6bdffcc190438036bd1
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-09 16:16:10 +0000

    Thu, Nov 9, 2023, 10:16 AM -06:00

---
+++ capseasy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+int main(void)
+{
+    string input1 = get_string("Before: ");
+    printf("After: ");
+    for (int i = 0; i < strlen[input1]; i++)
+    {
+        printf("%c \n", toupper(s[i]));
+    }
+}
