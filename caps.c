e0e4fd4a5c541bf4a2cda8ea1354c80dd85d049c
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-09 16:01:07 +0000

    Thu, Nov 9, 2023, 10:01 AM -06:00

--- caps.c
+++ caps.c
@@ -3,7 +3,10 @@
 #include <string.h>
 int main(void)
 {
-    string input = get_string("Before: \n");
+    string input1 = get_string("Before: \n");
     printf("After: \n");
-    
+    for(int i = 0; i < strlen(input1))
+    {
+        
+    }
 }
