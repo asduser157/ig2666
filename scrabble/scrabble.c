48b0b28e5a23fe017c2896ba4ee1e36e0c3bb64a
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-13 16:06:31 +0000

    Mon, Nov 13, 2023, 10:06 AM -06:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -4,9 +4,9 @@
 #include <string.h>
 
 // Points assigned to each letter of the alphabet
-int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+int pts[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int compute_score(string word);
+int compute(string word);
 
 int main(void)
 {
@@ -21,7 +21,7 @@ int main(void)
     // TODO: Print the winner
 }
 
-int compute_score(string word)
+int compute(string word)
 {
     // TODO: Compute and return score for string
 }
