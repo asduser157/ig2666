907ddd9cac477e1f5e74546a7557a3cc30658442
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-13 16:08:29 +0000

    Mon, Nov 13, 2023, 10:08 AM -06:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -13,6 +13,10 @@ int main(void)
     // Get input words from both players
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
+    for (int i = 0; i < strlen(word1); i++)
+    {
+        printf("%c", toupper(input1[i]));
+    }
 
     // Score both words
     int score1 = compute_score(word1);
