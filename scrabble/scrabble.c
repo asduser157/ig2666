e95bb33bc98e727fab2503761b10b31a09ec8f8b
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-13 16:34:09 +0000

    Mon, Nov 13, 2023, 10:34 AM -06:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,7 +6,11 @@
 // Points assigned to each letter of the alphabet
 int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
-char* getCharacters(char* str)
+char* getCharacters1(char* word1)
+{
+    // function implementation here
+}
+char* getCharacters2(char* word2)
 {
     // function implementation here
 }
@@ -20,8 +24,8 @@ int compute(string word);
 int main(void)
 {
     // Get input words from both players
-    string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
+    char* word1 = get_string("Player 1: ");
+    char* word2 = get_string("Player 2: ");
     char* character1 = getCharacters(word1);
     char* character2 = getCharacters(word2);
     for (int i = 0; i < strlen(word1); i++)
