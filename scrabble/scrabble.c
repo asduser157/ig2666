e4b16f1af7d658ea0a736a23abbf1e8043e0ae9a
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-14 16:09:40 +0000

    Tue, Nov 14, 2023, 10:09 AM -06:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -18,9 +18,23 @@ I = 1;
 J = 8;
 K = 5;
 L = 1;
+M = 3;
+N = 1;
+O = 1;
+P = 3;
+Q = 10;
+R = 1;
+S = 1;
+T = 1;
+U = 1;
+V = 4;
+W = 4;
+X = 8;
+Y = 4;
+Z = 10;
 char* getCharacters1(char* word1)
 {
-
+    
 }
 char* getCharacters2(char* word2)
 {
