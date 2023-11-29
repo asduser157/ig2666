9c8c608662dca8ae2d9f86bab91b7d1a17683ed7
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-29 16:09:29 +0000

    Wed, Nov 29, 2023, 10:09 AM -06:00

--- buzzfeed.c
+++ buzzfeed.c
@@ -1,1 +1,10 @@
-#incluce
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Welcome to my quiz! \n");
+    printf("Please type in the number associated with your answer. \n");
+
+    int question1 = get_int("What is your favorite)
+}
