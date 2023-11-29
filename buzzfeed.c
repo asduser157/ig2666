720bbe4b496d2e68ea3e51c54d00991861a07cfa
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-29 16:28:03 +0000

    Wed, Nov 29, 2023, 10:28 AM -06:00

--- buzzfeed.c
+++ buzzfeed.c
@@ -1,15 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
-int snow = 0;
-int sun = 0;
-int rain = 0;
-int cloud = 0;
-int main(void)
-{
-
-    printf("Welcome to my quiz! \n");
-    printf("Please type in the number associated with your answer. \n");
-
+int ask1()
+    {
+    int snow = 0;
+    int sun = 0;
+    int rain = 0;
+    int cloud = 0;
     int question1 = get_int("What is your favorite season? \n 1) Spring \n 2) Summer \n 3) Fall \n 4) Winter\n");
     if(question1 == 1)
     {
@@ -28,5 +24,16 @@ int main(void)
         snow++;
     }
     else
+    {
+        ask1();
+    }
+    }
+
+int main(void)
+{
+
+    printf("Welcome to my quiz! \n");
+    printf("Please type in the number associated with your answer. \n");
+    ask1();
 
 }
