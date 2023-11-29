e4987522f51207e7b1972ee2e209171b9d2a142b
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-29 16:11:57 +0000

    Wed, Nov 29, 2023, 10:11 AM -06:00

--- buzzfeed.c
+++ buzzfeed.c
@@ -3,8 +3,9 @@
 
 int main(void)
 {
+
     printf("Welcome to my quiz! \n");
     printf("Please type in the number associated with your answer. \n");
 
-    int question1 = get_int("What is your favorite)
+    int question1 = get_int("What is your favorite season? \n 1) Spring \n 2) Summer \n 3) Fall \n 4) Winter/n")
 }
