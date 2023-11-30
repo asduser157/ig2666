e124cd31c43fe3c3b1eaf2ea60001af7d752250c
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-30 16:17:13 +0000

    Thu, Nov 30, 2023, 10:17 AM -06:00

--- buzzfeed.c
+++ buzzfeed.c
@@ -25,7 +25,7 @@ void ask1()
     }
     else
     {
-        printf("Invalid number entered. Please try again.\n");
+        printf("Invalid number entered. Please try again.\n\n");
         ask1();
     }
     int question2 = get_int("Which best describes you? \n 1) I am generous, an when people see my true works they are amazed and thank me. \n 2) I try to be a positive person, and I appreciate the start of a new day.  \n 3) I am a creative person, and I strive to change so I can better help others. \n 4) I love helping others have fun, and I try to share the beauty in life with everyone.\n");
@@ -47,7 +47,7 @@ void ask1()
     }
     else
     {
-        printf("Invalid number entered. Please try again.\n");
+        printf("Invalid number entered. Please try again.\n\n");
         ask1();
     }
     int question3 = get_int("Which state would you prefer to live in? \n 1) Louisville, KY \n 2) Sacramento, CA \n 3) Seattle, WA \n 4) Buffalo, NY \n");
@@ -69,7 +69,51 @@ void ask1()
     }
     else
     {
-        printf("Invalid number entered. Please try again.\n");
+        printf("Invalid number entered. Please try again.\n\n");
+        ask1();
+    }
+    int question3 = get_int("Which shape do you like the most? \n 1) Triangle \n 2) Circle \n 3) Rectangle \n 4) Hexagon \n");
+    if(question4 == 1)
+    {
+        rain++;
+    }
+    else if(question4 == 2)
+    {
+        sun++;
+    }
+    else if(question4 == 3)
+    {
+        cloud++;
+    }
+    else if(question4 == 4)
+    {
+        snow++;
+    }
+    else
+    {
+        printf("Invalid number entered. Please try again.\n\n");
+        ask1();
+    }
+    int question5 = get_int("Which state would you prefer to live in? \n 1) Louisville, KY \n 2) Sacramento, CA \n 3) Seattle, WA \n 4) Buffalo, NY \n");
+    if(question5 == 1)
+    {
+        rain++;
+    }
+    else if(question5 == 2)
+    {
+        sun++;
+    }
+    else if(question5 == 3)
+    {
+        cloud++;
+    }
+    else if(question5 == 4)
+    {
+        snow++;
+    }
+    else
+    {
+        printf("Invalid number entered. Please try again.\n\n");
         ask1();
     }
 }
