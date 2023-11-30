2325c2746f042ad1488f7ebdb88febc339f8add3
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-11-30 16:30:07 +0000

    Thu, Nov 30, 2023, 10:30 AM -06:00

--- buzzfeed.c
+++ buzzfeed.c
@@ -72,7 +72,7 @@ void ask1()
         printf("Invalid number entered. Please try again.\n\n");
         ask1();
     }
-    int question3 = get_int("Which shape do you like the most? \n 1) Triangle \n 2) Circle \n 3) Rectangle \n 4) Hexagon \n");
+    int question4 = get_int("Which shape do you like the most? \n 1) Triangle \n 2) Circle \n 3) Rectangle \n 4) Hexagon \n");
     if(question4 == 1)
     {
         rain++;
@@ -94,7 +94,7 @@ void ask1()
         printf("Invalid number entered. Please try again.\n\n");
         ask1();
     }
-    int question5 = get_int("Which state would you prefer to live in? \n 1) Louisville, KY \n 2) Sacramento, CA \n 3) Seattle, WA \n 4) Buffalo, NY \n");
+    int question5 = get_int("What color do you most resonate with? \n 1) Blue or Purple \n 2) Red or Yellow \n 3) Gray or Pink \n 4) White or Black\n");
     if(question5 == 1)
     {
         rain++;
@@ -116,13 +116,29 @@ void ask1()
         printf("Invalid number entered. Please try again.\n\n");
         ask1();
     }
+    if(rain >= sun && rain >= cloud && rain >= snow)
+    {
+        printf("Congratulations! You have been sorted into the rain clan!");
+    }
+    else if(sun >= rain && sun >= cloud && sun >= snow)
+    {
+        printf("Congratulations! You have been sorted into the sun clan!");
+    }
+    else if(cloud >= sun && cloud >= rain && cloud >= snow)
+    {
+        printf("Congratulations! You have been sorted into the cloud clan!");
+    }
+    else if(snow >= sun && snow >= rain && snow >= cloud)
+    {
+        printf("Congratulations! You have been sorted into the snow clan!");
+    }
 }
 
 int main(void)
 {
 
-    printf("Welcome to my quiz! \n");
-    printf("Please type in the number associated with your answer. \n");
+    printf("\nWelcome to my quiz! Get ready to find out which clan you will join!\n\n");
+    printf("Please type in the number associated with your answer to the following questions. \n\n");
     ask1();
 
 }
