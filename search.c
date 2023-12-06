819ea256f814ddb8756c0c2ee3a242f3d65fc190
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-12-06 16:05:17 +0000

    Wed, Dec 6, 2023, 10:05 AM -06:00

--- search.c
+++ search.c
@@ -3,5 +3,11 @@
 #include<string.h>
 
 int main(void){
-    
+    string words[] = ("Grant", "Isaac", "Lauren", "Mason", "Kenzie", "Covey");
+    string name = get_string("What name are you looking for?");
+
+    for int index = 0; index < 0; index ++)
+    {
+        
+    }
 }
