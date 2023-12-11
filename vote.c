38303b353a7c1f5be88e54d74d2f2e1632e1503e
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-12-11 16:19:44 +0000

    Mon, Dec 11, 2023, 10:19 AM -06:00

---
+++ vote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void)
+{
+    int age = get_string("How old are you?\n");
+    int extra;
+    if(age >= 0 && age < 18)
+    {
+        extra = 18 - age;
+        printf("You are not eligible to vote. Please wait %i year(s)\n", extra);
+    }
+    else if(age == 18)
+    {
+        extra = 0;
+        printf("Congratulations! For the first time in your life, you are eligible to vote!\n")
+    }
+    else if(age > 18 && age < 110)
+    {
+        
+    }
+}
