53db68ae3eb374aa9160e09163c3dfd8e06d8aba
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-12-11 16:27:09 +0000

    Mon, Dec 11, 2023, 10:27 AM -06:00

--- vote.c
+++ vote.c
@@ -2,20 +2,27 @@
 #include <cs50.h>
 int main(void)
 {
-    int age = get_string("How old are you?\n");
-    int extra;
+    int age = get_int("How old are you?\n");
+    int extra = 0;
     if(age >= 0 && age < 18)
     {
         extra = 18 - age;
-        printf("You are not eligible to vote. Please wait %i year(s)\n", extra);
+        printf("You are not eligible to vote. Please wait %i year(s).\n", extra);
     }
     else if(age == 18)
     {
-        extra = 0;
-        printf("Congratulations! For the first time in your life, you are eligible to vote!\n")
+        printf("Congratulations! You have officialy reached the age of being eligible to vote!\n");
     }
-    else if(age > 18 && age < 110)
+    else if(age > 18 && age < 111)
     {
-        printf(")
+        printf("You are eligible to vote!");
+    }
+    else if(age > 110)
+    {
+        printf("You are dead. Please enter in a younger age so you are still alive.");
+    }
+    else if(age < 0)
+    {
+        printf("You have not been born yet. Please enter in an older age so you are alive.");
     }
 }
