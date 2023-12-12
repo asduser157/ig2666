d8c450ea1f041009d9e8a19f3fc258d6a0340c72
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-12-12 00:10:55 +0000

    Mon, Dec 11, 2023, 6:10 PM -06:00

--- vote.c
+++ vote.c
@@ -16,6 +16,36 @@ int main(void)
     {
         printf("Congratulations! You have officialy reached the age of being eligible to vote!\n");
         char vote = get_char("Who do you want to vote for?\n  a) Mickey Mouse \n  b) Winnie the Pooh \n  c) Bugs Bunny \n  d) Scooby Doo \n  e) Ozzy Osbourne \n");
+        char vote = get_char("Who do you want to vote for?\n  a) Mickey Mouse \n  b) Winnie the Pooh \n  c) Bugs Bunny \n  d) Scooby Doo \n  e) Ozzy Osbourne \n  f) Yourself \n");
+        if(vote == 'a')
+        {
+            printf("Eek! Walt Disney was afraid of mice. Hold on a second to see if Mickey wins!\n");
+        }
+        else if(vote == 'b')
+        {
+            printf("Winnie the Pooh is the patron saint of teddy bears! Let's see if Winnie wins!\n");
+        }
+        else if(vote == 'c')
+        {
+            printf("The actor for Bugs Bunny was allergic to carrots! Wait to see if Bugs wins!\n");
+        }
+        else if(vote == 'd')
+        {
+            printf("Scooby-Doo's real name is Scoobert. Let's hold on and see if Scooby-Doo wins!\n");
+        }
+        else if(vote == 'e')
+        {
+            printf("You're going off the rails on a crazy train! Stay to find out if Ozzy wins!\n");
+        }
+        else
+        {
+            printf("Uh-oh! Your vote was not valid. I guess Ozzy Osbourne got an extra unexpected vote!\n");
+        }
+        char *array1[5] = {"Mickey Mouse!", "Winnie the Pooh!", "Bugs Bunny!", "Scooby-Doo!", "Ozzy Osbourne!",};
+        int randomIndex = rand() % 5;
+        char *randomElement = array1[randomIndex];
+        printf("\nThe winner is............................. ");
+        printf("%s\n", randomElement);
     }
     else if(age > 18 && age < 111 && age != 35)
     {
@@ -30,23 +60,23 @@ int main(void)
         {
             printf("Eek! Walt Disney was afraid of mice. Hold on a second to see if Mickey wins!\n");
         }
-        if(vote == 'b')
+        else if(vote == 'b')
         {
             printf("Winnie the Pooh is the patron saint of teddy bears! Let's see if Winnie wins!\n");
         }
-        if(vote == 'c')
+        else if(vote == 'c')
         {
             printf("The actor for Bugs Bunny was allergic to carrots! Wait to see if Bugs wins!\n");
         }
-        if(vote == 'd')
+        else if(vote == 'd')
         {
             printf("Scooby-Doo's real name is Scoobert. Let's hold on and see if Scooby-Doo wins!\n");
         }
-        if(vote == 'e')
+        else if(vote == 'e')
         {
             printf("You're going off the rails on a crazy train! Stay to find out if Ozzy wins!\n");
         }
-        if(vote == 'f')
+        else if(vote == 'f')
         {
             printf("You are in the mix of candidates! Let's wait for a bit and see if you win!\n");
         }
