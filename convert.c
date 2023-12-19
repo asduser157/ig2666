54ca67640f6cdc86fc4dce8ed8562585b18e1411
ig2666 <112423102+ig2666@users.noreply.github.com>
2023-12-19 17:07:30 +0000

    Tue, Dec 19, 2023, 11:07 AM -06:00

--- convert.c
+++ convert.c
@@ -4,15 +4,14 @@
 int main(void)
 {
     int decide = 0;
-    double Fahrenheit = 0;
-    double Celsius = 0;
-    double Kelvin = 0;
+    double Fahrenheit;
+    double Celsius;
+    double Kelvin;
     printf("Welcome to the temperature converter engine!\n\n");
     Fahrenheit = get_double("Please type in a temperature in degrees Fahrenheit. \nNon-numerical characters (besides properly placed decimal points) will not be accepted. \n\nType it here: ");
     decide = get_int("_______________________________________________________________________________________\n\nWhich unit would you like to convert your temperature to?\n 1) Celsius \n 2) Kelvin \n");
     if (decide == 1)
     {
-        Kelvin = 0;
         Fahrenheit = Fahrenheit - 32;
         Celsius = (Fahrenheit * (5/9));
         printf("_______________________________________________________________________________________\n\nYour final temperature is: %lf °C \n", Celsius);
