65df123f91de6af31615f9d879171bdbc8ccd44b
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-03-27 15:24:49 +0000

    Wed, Mar 27, 2024, 10:24 AM -05:00

--- wc.py
+++ wc.py
@@ -1,4 +1,5 @@
 white_collar = ["Neal Caffrey", "Peter Burke", "Jones", "Elizabeth Burke", "Diana Berrigan", "Mozzie", "Sara Ellis", "Kate Moreau", "Matthew Keller"]
 white_collar.append("Mozzie")
+
 white_collar.sort()
 print(white_collar)
