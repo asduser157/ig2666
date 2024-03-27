8e31042b3204c75515d4ded9ae34e157b0dee814
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-03-27 15:34:16 +0000

    Wed, Mar 27, 2024, 10:34 AM -05:00

--- wc.py
+++ wc.py
@@ -1,7 +1,7 @@
 white_collar = ["Neal Caffrey", "Garrett Fowler", "Alex Hunter", "Peter Burke", "Jones", "Elizabeth Burke", "Diana Berrigan", "Mozzie", "Sara Ellis", "Kate Moreau", "Matthew Keller"]
 white_collar.append("Curtis Hagen")
 print(white_collar)
-white_collar.clear("Matthew Keller")
+white_collar.clear()
 print(white_collar)
 white_collar.sort()
 print(white_collar)
