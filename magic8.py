044e42ef24eec55d4cef197d751671ca0a248414
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-03-29 00:08:49 +0000

    Thu, Mar 28, 2024, 7:08 PM -05:00

--- magic8.py
+++ magic8.py
@@ -1,2 +1,3 @@
 import random
-
+num = random.randint(1,9)
+response['Yes - definitely.', 'It is decidedly so.', 'Without a doubt.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
