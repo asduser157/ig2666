76a28f4666a734f48a3e40efc355c7a96a4945de
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-03-29 14:38:04 +0000

    Fri, Mar 29, 2024, 9:38 AM -05:00

--- magic8.py
+++ magic8.py
@@ -1,6 +1,6 @@
 import random
 question = input("Ask the Magic 8 Ball a Yes-or-No Question:\n")
-response = ['Yes - definitely.', 'It is decidedly so.', 'Without a doubt.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
+response = ['Yes, definitely', 'It is certain.', 'Affirmative.', 'Reply hazy, try again.', 'Time will tell.', 'Better not tell you now.', 'My sources say no.', 'Negative.', 'Nope, not at all.']
 num = random.randint(1,9)
 answer = random.choice(response)
 print(answer)
