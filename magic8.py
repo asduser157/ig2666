2e93bccd0979a32e45c1dbf992c4163b0d83eb80
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-03-29 00:15:20 +0000

    Thu, Mar 28, 2024, 7:15 PM -05:00

--- magic8.py
+++ magic8.py
@@ -2,4 +2,6 @@ import random
 question = input("Ask the Magic 8 Ball a Question to Receive an Answer")
 response['Yes - definitely.', 'It is decidedly so.', 'Without a doubt.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
 num = random.randint(1,9)
-if num = 1()
+if num == 1:
+    print
+
