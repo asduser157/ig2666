ced74706afcf64b3947ecbd8148346b2c6a9f3b2
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-03-31 00:55:42 +0000

    Sat, Mar 30, 2024, 7:55 PM -05:00

--- madlib.py
+++ madlib.py
@@ -3,6 +3,6 @@ ani1 = input("\nAnimal: ")
 vb1 = input("\nPresent-Tense Verb: ")
 col1 = input("\nColor:")
 vb2 = input("\nAnother Present-Tense Verb:")
-
+ani2 = input
 print("There once was a/an %i who loved to %i anything the color %i. One day, the %i decided to %i a/an %i it saw walking near %i )
 
