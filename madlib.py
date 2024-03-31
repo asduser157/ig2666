acbeb9bf223e7857572f092af1fa7fb5264bad51
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-03-31 00:54:39 +0000

    Sat, Mar 30, 2024, 7:54 PM -05:00

--- madlib.py
+++ madlib.py
@@ -1,5 +1,8 @@
 import random
 ani1 = input("\nAnimal: ")
-vb1 = input("\nPresent Tense Verb: ")
+vb1 = input("\nPresent-Tense Verb: ")
+col1 = input("\nColor:")
+vb2 = input("\nAnother Present-Tense Verb:")
+
 print("There once was a/an %i who loved to %i anything the color %i. One day, the %i decided to %i a/an %i it saw walking near %i )
 
