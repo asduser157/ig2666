2e00c12d370dea67ce9414306c4bc3a7e823cae7
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-04-04 15:17:51 +0000

    Thu, Apr 4, 2024, 10:17 AM -05:00

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 football = {"Arizona":"Cardinals", "Atlanta":"Falcons", "Baltimore":"Ravens", "Buffalo":"Bills", "Carolina":"Panthers", "Cincinatti":"Bengals", "Cleveland":"Browns", "Chicago":"Bears",  }
 name = {"First_name":"Isaac", "Last_name":"Grandy"}
-emoji = {"Happy": "😃", "Sad":"😭", "Silly":"🤪", "Angry":"😡"}
-print(emoji)
+emoji = {"Happy":"😃", "Sad":"😭", "Silly":"🤪", "Angry":"😡"}
+emoji["Dead":"😵"]
+Anime_Sucks = {"Giyu Tomioka"}
