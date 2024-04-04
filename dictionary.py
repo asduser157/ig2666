07598b680889f17f0b252419a96bac5681379ce4
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-04-04 15:06:46 +0000

    Thu, Apr 4, 2024, 10:06 AM -05:00

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,1 @@
-football = {"Arizona":"Cardinals", "Atlanta":"Falcons", "Baltimore":"Ravens", "Buffalo":"Bills", "Carolina":"Panthers", "Cincinatti":"Bengals", "Cleveland":"Browns", "Chicago":"Bears", }
+football = {"Arizona":"Cardinals", "Atlanta":"Falcons", "Baltimore":"Ravens", "Buffalo":"Bills", "Carolina":"Panthers", "Cincinatti":"Bengals", "Cleveland":"Browns", "Chicago":"Bears",  }
