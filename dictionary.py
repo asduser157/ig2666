c4c968dc5b4a7ca60ff2a5c94ec3e614fee024c6
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-04-04 15:25:28 +0000

    Thu, Apr 4, 2024, 10:25 AM -05:00

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,11 @@
 #football = {"Arizona":"Cardinals", "Atlanta":"Falcons", "Baltimore":"Ravens", "Buffalo":"Bills", "Carolina":"Panthers", "Cincinatti":"Bengals", "Cleveland":"Browns", "Chicago":"Bears",  }
 name = {"First_name":"Isaac", "Last_name":"Grandy"}
 emoji = {"Happy":"😃", "Sad":"😭", "Silly":"🤪", "Angry":"😡"}
-emoji["Dead":"😵"]
+emoji["Dead"] = "😵"
 Anime_Sucks = {"Giyu Tomioka":"🌊 Hashira", "Mitsuri Kanroji":"💗 Hashira", "Kyojuro Rengoku":"🔥 Hashira"}
-del Anime_Sucks =["Mitsuri Kanroji"]
+del Anime_Sucks ["Mitsuri Kanroji"]
+def clear_dict(Anime_Sucks):
+    Anime_Sucks.clear()
 
 
 print(name)
