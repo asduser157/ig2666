bafbfa2d040fd1fdbdebfaa81ebbca1851396a86
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-04-04 15:23:01 +0000

    Thu, Apr 4, 2024, 10:23 AM -05:00

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,11 @@
-football = {"Arizona":"Cardinals", "Atlanta":"Falcons", "Baltimore":"Ravens", "Buffalo":"Bills", "Carolina":"Panthers", "Cincinatti":"Bengals", "Cleveland":"Browns", "Chicago":"Bears",  }
+#football = {"Arizona":"Cardinals", "Atlanta":"Falcons", "Baltimore":"Ravens", "Buffalo":"Bills", "Carolina":"Panthers", "Cincinatti":"Bengals", "Cleveland":"Browns", "Chicago":"Bears",  }
 name = {"First_name":"Isaac", "Last_name":"Grandy"}
 emoji = {"Happy":"😃", "Sad":"😭", "Silly":"🤪", "Angry":"😡"}
 emoji["Dead":"😵"]
-Anime_Sucks = {"Giyu Tomioka"}
+Anime_Sucks = {"Giyu Tomioka":"🌊 Hashira", "Mitsuri Kanroji":"💗 Hashira", "Kyojuro Rengoku":"🔥 Hashira"}
+del Anime_Sucks =["Mitsuri Kanroji"]
+
+
+print(name)
+print(emoji)
+print(Anime_Sucks)
