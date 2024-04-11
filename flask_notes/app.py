fc431bfef2d1fa422cb363d1b3813acf5758c415
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-04-11 15:28:28 +0000

    Thu, Apr 11, 2024, 10:28 AM -05:00

--- flask_notes/app.py
+++ flask_notes/app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def flask():
     return render_template("flask.html")
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html" name=request.args.get("name", "world"))
