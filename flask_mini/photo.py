7709ea7d7cc8e3321327ab69c005017ed563835d
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-04-17 15:07:56 +0000

    Wed, Apr 17, 2024, 10:07 AM -05:00

--- flask_mini/photo.py
+++ flask_mini/photo.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route("/")
 def flask():
-    return render_template("index.html")
+    return render_template("photo1.html")
 
 @app.route("/greet")
 def greet():
-    return render_template("greet.html", name=request.args.get("name", "world"))
+    return render_template("photo2.html", name=request.args.get("name", "world"))
