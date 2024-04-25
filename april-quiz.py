7971dcd544235202a7f2eed463a7cd67029dc21e
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-04-25 15:26:01 +0000

    Thu, Apr 25, 2024, 10:26 AM -05:00

--- april-quiz.py
+++ april-quiz.py
@@ -5,13 +5,29 @@ greet = input("What is your first name?\n")
 print("Hello there, %s!" % (greet))
 
 # Level 1
-#asks user for desired math function
+# asks user for desired math function
 calc = input("Type '+' for addition, '-' for subtraction, '*' for multiplication, or '/' for division:\n")
-#asks user for first number in equation
-int num1 = get_int("Enter in the first number:\n")
-#asks user for second number in equation
-int num2 = get_int("Enter in the second number:\n")
+# asks user for first number in equation
+num1 = int(input("Enter in the first number:\n"))
+# asks user for second number in equation
+num2 = int(input("Enter in the second number:\n"))
+# adds two numbers if addition is selected
 if (calc == "+"):
     print("%i + %i: %i" % (num1, num2,(num1+num2)))
+# subtracts two numbers if addition is selected
+elif (calc == "-"):
+    print("%i - %i: %i" % (num1, num2,(num1-num2)))
+# multiplies two numbers if multiplication is selected
+elif (calc == "*"):
+    print("%i * %i: %i" % (num1, num2,(num1*num2)))
+# divided two numbers if division is selected
+elif (calc == "/"):
+    print("%i / %i: %i" % (num1, num2,(num1/num2)))
+#gives user instructions if user mistake is made
+else:
+    print("Incorrect math function. Please press CTRL+C to rerun program and try again.")
+
+# Level 2
+
 
 
