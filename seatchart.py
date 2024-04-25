663dab533fb7680661df7341991fd00c3dbfc84e
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-04-25 15:01:56 +0000

    Thu, Apr 25, 2024, 10:01 AM -05:00

--- seatchart.py
+++ seatchart.py
@@ -1,13 +1,43 @@
 import random
 import time
-seat = ["Lauren", "Mason", "Greer", "Covey", "Simon", "Hayden", "Grant", "Dashell", "Lea", "Isaac", "Sariah", "Vicko", "Anna", "Ariah", "", "", "", "", "", "", "", "", "", "", "", ""]
+seat = ["Lauren", "Mason", "Greer", "Covey", "Simon", "Hayden", "Grant", "Dashell", "Lea", "Isaac", "Sariah", "Vicko", "Anna", "Ariah", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
 num = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28"]
 
-for i in range(26):
-    s = random.choice(seat)
-    n = random.choice(num)
-    time.sleep(0.6)
-    print("%s: %s\n" % (n, s))
-    seat.remove(s)
-    num.remove(n)
+def please():
+    for i in range(27):
+        for item in num:
+            s = random.choice(seat)
+            time.sleep(0)
+            print("%s: %s\n" % (item, s))
+            seat.remove(s)
+        if item == "28":
+            break
+    if s == "Mason":
+        for int in range(6):
+            int = int - 1
+        if int > 0:
+            print("\n" * 30)
+            please()
+    if s == "Simon":
+        for int in range(6):
+            int = int - 1
+        if int > 0:
+            print("\n" * 30)
+            please()
+    if s == "Hayden":
+        for int in range(6):
+            int = int - 1
+        if int > 0:
+            print("\n" * 30)
+            please()
+    if s == "Vicko":
+        for int in range(6):
+            int = int - 1
+        if int > 0:
+            print("\n" * 30)
+            please()
+
+
+
+please()
 
