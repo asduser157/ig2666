7d4862ed728c7b51b9743ca17ce53852245f5d36
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-04-25 17:57:47 +0000

    Thu, Apr 25, 2024, 12:57 PM -05:00

--- april-quiz.py
+++ april-quiz.py
@@ -35,16 +35,56 @@ rand = random.randint(1,20)
 # sets a trigger
 correct = False
 print("The computer has randomly selected a number between 1 and 20. Guess it correctly to move forward.\n")
-#while trigger is not activated, run this code:
+# while trigger is not activated, run this code:
 while (correct == False):
+    # have the user guess
     guess = int(input("Type your guess here:\n"))
+    #if guess is too high, tell user to guess lower
     if (guess > rand):
         toohigh = int(input("Guess lower! Type here:\n"))
-    if (guess < rand):
+    #if guess is too low, tell user to guess higher
+    elif (guess < rand):
         toolow = int(input("Guess higher! Type here:\n"))
-    if (guess == rand):
+    #if guess is correct, let the user know
+    elif (guess == rand):
         print("Correct! The number was %s." % (rand))
+        #stop the while loop via a trigger
         correct = True
+    else:
+        print("ERROR: Please type an integer. Press CTRL+C to rerun program and try again.")
+
+
+# Level 3
+# player1's guess
+player1 = input("PLAYER 1- Type in 'r' for rock, 'p' for paper, or 's' for scissors:\n")
+# confirmation
+print("PLAYER 1- Your move has been recorded.\n\n\n\n\n\n\n\n\n\n\n")
+# player2's guess
+player2 = input("PLAYER 2- Type in 'r' for rock, 'p' for paper, or 's' for scissors:\n")
+# confirmation
+print("PLAYER 2- Your move has been recorded.\n\n\n\n\n\n\n\n\n\n\n")
+# checking outcome...
+if (player1 == "r" & player2 == "s"):
+    print("Rock beats scissors! PLAYER 1 wins!")
+elif (player1 == "p" & player2 == "s"):
+    print("Scissors beats paper! PLAYER 2 wins!")
+elif (player1 == "s" & player2 == "s"):
+    print("Scissors ties scissors! It's a draw!")
+elif (player1 == "r" & player2 == "p"):
+    print("Paper beats rock! PLAYER 2 wins!")
+elif (player1 == "s" & player2 == "p"):
+    print("Scissors beats paper! PLAYER 1 wins!")
+elif (player1 == "p" & player2 == "p"):
+    print("Paper ties paper! It's a draw!")
+elif (player1 == "r" & player2 == "r"):
+    print("Rock ties rock! It's a draw!")
+elif (player1 == "p" & player2 == "r"):
+    print("Paper beats rock! PLAYER 1 wins!")
+elif (player1 == "s" & player2 == "r"):
+    print("Rock beats scissors! PLAYER 2 wins!")
+# making sure that type is correct
+else:
+    print("ERROR: Please ensure both players have only type 'r', 's', or 'p'. Press CTRL+C to rerun program and try again.")
 
 
 
