68b216d5bc60c879f99f498fa40d9b2303ce5065
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-04-25 15:36:44 +0000

    Thu, Apr 25, 2024, 10:36 AM -05:00

--- april-quiz.py
+++ april-quiz.py
@@ -1,3 +1,5 @@
+#imports the random library
+import random
 # Level 0
 # asks user for their name
 greet = input("What is your first name?\n")
@@ -13,21 +15,38 @@ num1 = int(input("Enter in the first number:\n"))
 num2 = int(input("Enter in the second number:\n"))
 # adds two numbers if addition is selected
 if (calc == "+"):
-    print("%i + %i: %i" % (num1, num2,(num1+num2)))
+    print("%i+%i = %i" % (num1, num2,(num1+num2)))
 # subtracts two numbers if addition is selected
 elif (calc == "-"):
-    print("%i - %i: %i" % (num1, num2,(num1-num2)))
+    print("%i-%i = %i" % (num1, num2,(num1-num2)))
 # multiplies two numbers if multiplication is selected
 elif (calc == "*"):
-    print("%i * %i: %i" % (num1, num2,(num1*num2)))
-# divided two numbers if division is selected
+    print("%i*%i = %i" % (num1, num2,(num1*num2)))
+# divides two numbers if division is selected
 elif (calc == "/"):
-    print("%i / %i: %i" % (num1, num2,(num1/num2)))
-#gives user instructions if user mistake is made
+    print("%i/%i = %i" % (num1, num2,(num1/num2)))
+# gives user instructions if user mistake is made
 else:
     print("Incorrect math function. Please press CTRL+C to rerun program and try again.")
 
 # Level 2
+# sets the random number
+rand = random.randint(1,20)
+# sets a trigger
+correct = False
+print("The computer has randomly selected a number between 1 and 20. Guess it correctly to move forward.\n")
+#while trigger is not activated, run this code:
+while (correct == False):
+    guess = int(input("Type your guess here:\n"))
+    if (guess > rand):
+        toohigh = int(input("Guess lower! Type here:\n"))
+    if (guess < rand):
+        toolow = int(input("Guess higher! Type here:\n"))
+    if (guess == rand):
+        print("Correct!")
+        correct = True
+
+
 
 
 
