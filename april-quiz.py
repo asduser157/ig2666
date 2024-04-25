2b96d3d089f62e22b01561547229fa4033374558
ig2666 <112423102+ig2666@users.noreply.github.com>
2024-04-25 15:16:13 +0000

    Thu, Apr 25, 2024, 10:16 AM -05:00

--- april-quiz.py
+++ april-quiz.py
@@ -1,3 +1,17 @@
-# No.1
+# Level 0
+# asks user for their name
 greet = input("What is your first name?\n")
-print("Hello there, %" % (greet))
+# greets user w/ their name
+print("Hello there, %s!" % (greet))
+
+# Level 1
+#asks user for desired math function
+calc = input("Type '+' for addition, '-' for subtraction, '*' for multiplication, or '/' for division:\n")
+#asks user for first number in equation
+num1 = input("Enter in the first number:\n")
+#asks user for second number in equation
+num2 = input("Enter in the second number:\n")
+if (calc == "+"):
+    print("%inum1+num2)
+
+
